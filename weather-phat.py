2280929a586c6c666478ec852a0e8d0c4127aa38
Steve <stephen@sjefferson.co.uk>
2023-05-10 00:39:27 +0100

    Some example text layout

--- weather-phat.py
+++ weather-phat.py
@@ -9,7 +9,7 @@ from sys import exit
 import config
 from open_meteo import weather_api
 
-from font_fredoka_one import FredokaOne
+#from font_fredoka_one import FredokaOne
 from inky import InkyPHAT
 from PIL import Image, ImageDraw, ImageFont
 
@@ -34,7 +34,7 @@ if inky_display.resolution not in ((212, 104), (250, 122)):
     w, h = inky_display.resolution
     raise RuntimeError("This example does not support {}x{}".format(w, h))
 
-inky_display.set_border(inky_display.BLACK)
+#inky_display.set_border(inky_display.BLACK)
 
 def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_display.RED)):
     """Create a transparency mask.
@@ -73,15 +73,18 @@ weather_icon = None
 
 if weatherdata:
     pressure = weatherdata["pressure"]
+    pressure2 = pressure - 3
     #temperature = weatherdata["temperature"]
-    windspeed = weatherdata["wind_speed"]
+    wind_speed = weatherdata["wind_speed"]
     weathercode = weatherdata["weather_icon"]
 
 else:
     print("Warning, no weather information found!")
 
 # Create a new canvas to draw on
-img = Image.open(os.path.join(PATH, "resources/backdrop.png")).resize(inky_display.resolution)
+#img = Image.open(os.path.join(PATH, "resources/backdrop.png")).resize(inky_display.resolution)
+#draw = ImageDraw.Draw(img)
+img = Image.new("P", (inky_display.resolution))
 draw = ImageDraw.Draw(img)
 
 # Load our icon files and generate masks
@@ -92,33 +95,41 @@ for icon in glob.glob(os.path.join(PATH, "resources/icon-*.png")):
    masks[icon_name] = create_mask(icon_image)
 
 # Load the FredokaOne font
-font = ImageFont.truetype(FredokaOne, 22)
+#font = ImageFont.truetype(FredokaOne, 15)
+# https://webpagepublicity.com/free-fonts-e.html#FreeFonts
+bigfont = ImageFont.truetype("EdenMills.ttf", 16)
+littlefont = ImageFont.truetype("EdenMills.ttf", 12)
 
 # Draw lines to frame the weather data
-draw.line((69, 36, 69, 81))       # Vertical line
-draw.line((31, 35, 184, 35))      # Horizontal top line
-draw.line((69, 58, 174, 58))      # Horizontal middle line
-draw.line((169, 58, 169, 58), 2)  # Red seaweed pixel :D
+draw.line(((inky_display.resolution[0] / 2), 2, (inky_display.resolution[0] / 2), inky_display.resolution[1] - 2), inky_display.RED) # Centre division
+#draw.line((31, 35, 184, 35))      # Horizontal top line
+#draw.line((69, 58, 174, 58))      # Horizontal middle line
+#draw.line((169, 58, 169, 58), 2)  # Red seaweed pixel :D
 
 # Write text with weather values to the canvas
 datetime = time.strftime("%d/%m %H:%M")
+time2 = time.time() - 3*60*60
+datetime2 = time.strftime("%d/%m %H:%M", time.localtime(time2))
 
-draw.text((41, 12), datetime, inky_display.WHITE, font=font)
+draw.text((0, 0), datetime, inky_display.BLACK, font=bigfont)
 
 # draw.text((72, 34), "T", inky_display.WHITE, font=font)
-draw.text((72, 34), "P", inky_display.WHITE, font=font)
+draw.text((0, 17), "{} HPa".format(pressure), inky_display.RED, font=bigfont)
+draw.text((0, 34), "{} HPa".format(pressure), inky_display.BLACK, font=littlefont)
 # draw.text((92, 34), "{}°C".format(temperature), inky_display.WHITE, font=font)
-draw.text((92, 34), "{}".format(pressure), inky_display.WHITE, font=font)
+draw.text((0, 47), "{} kts".format(wind_speed), inky_display.RED, font=bigfont)
+draw.text((0, 64), "{}N, {}E".format(round(latlong[0], 4), round(latlong[1], 4)), inky_display.BLACK, font=littlefont)
+draw.text((((inky_display.resolution[0] / 2) + 5), 0), datetime2, inky_display.RED, font=bigfont)
 
-draw.text((72, 58), "W", inky_display.WHITE, font=font)
-draw.text((92, 58), "{}kmh".format(windspeed), inky_display.WHITE, font=font)
+#draw.text((72, 58), "W", inky_display.WHITE, font=font)
+#draw.text((92, 58), "{}kmh".format(windspeed), inky_display.WHITE, font=font)
 
 # Draw the current weather icon over the backdrop
-if weather_icon is not None:
-   img.paste(icons[weather_icon], (28, 36), masks[weather_icon])
+#if weather_icon is not None:
+#   img.paste(icons[weather_icon], (28, 36), masks[weather_icon])
 
-else:
-   draw.text((28, 36), "?", inky_display.RED, font=font)
+#else:
+#   draw.text((28, 36), "?", inky_display.RED, font=font)
 
 # Display the weather data on Inky pHAT
 if enable_display:
