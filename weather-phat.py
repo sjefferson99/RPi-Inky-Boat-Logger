67f14a19490c5efd183643cc5cfb697fa670f3e0
Steve <stephen@sjefferson.co.uk>
2023-05-11 00:24:56 +0100

    Populated display

--- weather-phat.py
+++ weather-phat.py
@@ -7,8 +7,6 @@ import time
 from sys import exit
 import config
 from open_meteo import weather_api
-
-#from font_fredoka_one import FredokaOne
 from inky import InkyPHAT
 from PIL import Image, ImageDraw, ImageFont
 
@@ -16,11 +14,6 @@ from PIL import Image, ImageDraw, ImageFont
 enable_display = True
 
 offset_hours = config.offset_hours
-
-# Details to customise your weather display
-
-print("Boat logger")
-print("Displays weather information for a given location in the config file:")
 
 # Get the current path
 PATH = os.path.dirname(__file__)
@@ -34,8 +27,6 @@ except TypeError:
 if inky_display.resolution not in ((212, 104), (250, 122)):
     w, h = inky_display.resolution
     raise RuntimeError("This example does not support {}x{}".format(w, h))
-
-#inky_display.set_border(inky_display.BLACK)
 
 def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_display.RED)):
     """Create a transparency mask.
@@ -66,26 +57,7 @@ weather = weather_api()
 latlong = weather.get_latlong(config.city, config.countrycode)
 weatherdata = weather.get_weather(latlong, offset_hours)
 
-# Placeholder variables
-pressure = 0.0
-wind_speed = 0.0
-wind_gusts = 0.0
-wind_direction = 0.0
-temperature = 0.0
-weather_icon = None
-
-if weatherdata:
-    #pressure = weatherdata["pressure"]
-    temperature = weatherdata["temperature"]
-    #wind_speed = weatherdata["wind_speed"]
-    weathercode = weatherdata["weather_icon"]
-
-else:
-    print("Warning, no weather information found!")
-
 # Create a new canvas to draw on
-#img = Image.open(os.path.join(PATH, "resources/backdrop.png")).resize(inky_display.resolution)
-#draw = ImageDraw.Draw(img)
 img = Image.new("P", (inky_display.resolution))
 draw = ImageDraw.Draw(img)
 
@@ -96,17 +68,12 @@ for icon in glob.glob(os.path.join(PATH, "resources/icon-*.png")):
    icons[icon_name] = icon_image
    masks[icon_name] = create_mask(icon_image)
 
-# Load the FredokaOne font
-#font = ImageFont.truetype(FredokaOne, 15)
 # https://webpagepublicity.com/free-fonts-e.html#FreeFonts
-bigfont = ImageFont.truetype("EdenMills.ttf", 16)
+bigfont = ImageFont.truetype("EdenMills.ttf", 14)
 littlefont = ImageFont.truetype("EdenMills.ttf", 12)
 
-# Draw lines to frame the weather data
+# Draw line to separate the weather data
 draw.line(((inky_display.resolution[0] / 2), 2, (inky_display.resolution[0] / 2), inky_display.resolution[1] - 2), inky_display.RED) # Centre division
-#draw.line((31, 35, 184, 35))      # Horizontal top line
-#draw.line((69, 58, 174, 58))      # Horizontal middle line
-#draw.line((169, 58, 169, 58), 2)  # Red seaweed pixel :D
 
 # Write text with weather values to the canvas
 datetime = time.strftime("%d/%m %H:%M")
@@ -114,17 +81,24 @@ time2 = time.time() - offset_hours*60*60
 datetime2 = time.strftime("%d/%m %H:%M", time.localtime(time2))
 
 draw.text((0, 0), datetime, inky_display.BLACK, font=bigfont)
+draw.text((0, 15), "{} C".format(weatherdata["temperature"]), inky_display.BLACK, font=littlefont)
+draw.text((0, 28), "{} hPa".format(weatherdata["pressure"]), inky_display.BLACK, font=littlefont)
+draw.text((0, 41), "{} kts".format(weatherdata["wind_speed"]), inky_display.BLACK, font=littlefont)
+draw.text((0, 54), "{} kts Gust".format(weatherdata["wind_gusts"]), inky_display.BLACK, font=littlefont)
+draw.text((0, 67), "{} Deg".format(weatherdata["wind_direction"]), inky_display.BLACK, font=littlefont)
+draw.text((0, 80), "{} C Dew".format(weatherdata["dewpoint"]), inky_display.BLACK, font=littlefont)
+draw.text((0, 93), "{}N, {}E".format(latlong[0], latlong[1]), inky_display.BLACK, font=littlefont)
+img.paste(icons[weatherdata["weather_icon"]], (55, 10), masks[weatherdata["weather_icon"]])
 
-# draw.text((72, 34), "T", inky_display.WHITE, font=font)
-draw.text((0, 17), "{} C".format(temperature), inky_display.RED, font=bigfont)
-#draw.text((0, 34), "{} hPa".format(pressure), inky_display.BLACK, font=littlefont)
-# draw.text((92, 34), "{}°C".format(temperature), inky_display.WHITE, font=font)
-#draw.text((0, 47), "{} kts".format(wind_speed), inky_display.RED, font=bigfont)
-draw.text((0, 64), "{}N, {}E".format(round(latlong[0], 4), round(latlong[1], 4)), inky_display.BLACK, font=littlefont)
 draw.text((((inky_display.resolution[0] / 2) + 5), 0), datetime2, inky_display.RED, font=bigfont)
-
-#draw.text((72, 58), "W", inky_display.WHITE, font=font)
-#draw.text((92, 58), "{}kmh".format(windspeed), inky_display.WHITE, font=font)
+draw.text((((inky_display.resolution[0] / 2) + 5), 15), "{} C".format(weatherdata["offset_temperature"]), inky_display.RED, font=littlefont)
+draw.text((((inky_display.resolution[0] / 2) + 5), 28), "{} hPa".format(weatherdata["offset_pressure"]), inky_display.RED, font=littlefont)
+draw.text((((inky_display.resolution[0] / 2) + 5), 41), "{} kts".format(weatherdata["offset_wind_speed"]), inky_display.RED, font=littlefont)
+draw.text((((inky_display.resolution[0] / 2) + 5), 54), "{} kts Gust".format(weatherdata["offset_wind_gusts"]), inky_display.RED, font=littlefont)
+draw.text((((inky_display.resolution[0] / 2) + 5), 67), "{} Deg".format(weatherdata["offset_wind_direction"]), inky_display.RED, font=littlefont)
+draw.text((((inky_display.resolution[0] / 2) + 5), 80), "{} C Dew".format(weatherdata["offset_dewpoint"]), inky_display.RED, font=littlefont)
+draw.text((((inky_display.resolution[0] / 2) + 5), 93), "{}N, {}E".format(latlong[0], latlong[1]), inky_display.RED, font=littlefont)
+img.paste(icons[weatherdata["offset_weather_icon"]], (165, 10), masks[weatherdata["offset_weather_icon"]])
 
 # Draw the current weather icon over the backdrop
 #if weather_icon is not None:
